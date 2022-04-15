fc1c99b4e1db3f3df127221b570db25ab92d7f85
N I N K <69116944+ninksazerac@users.noreply.github.com>
2022-04-16 02:50:07 +0700

    update

---
+++ src/page/Paymentcompany/index.js
@@ -0,0 +1,169 @@
+import React from "react";
+import Typography from '@material-ui/core/Typography';
+import { ThemeProvider, createTheme } from '@material-ui/core/styles';
+import "./Paymentcompany.css";
+import qr from "../../assets/pics/QR_code_for_mobile_English_Wikipedia.svg.webp";
+
+export default function Paymentcompany(){
+    const notosan1=createTheme({
+        typography:{
+          subtitle1:{
+            fontSize:35,
+            fontWeight:'bold',
+            fontFamily: [
+              'Noto Sans Thai',
+              'sans-serif',
+            ].join(','),
+          },
+          body1:{
+            fontSize:25,
+            fontFamily: [
+              'Noto Sans Thai',
+              'sans-serif',
+            ].join(','),
+          },
+          body2:{
+            fontSize:18,
+            fontWeight:500,
+            fontFamily: [
+              'Noto Sans Thai',
+              'sans-serif',
+            ].join(','),
+          }
+        },
+      });
+    return(
+        <ThemeProvider theme={notosan1}>
+            <div className=" h-20 w-200 bg-green-300 rounded-t-lg mx-10 mt-8">
+
+            {/* หัวข้อ */}
+            <div div className="flex items-center justify-center pt-2.5">
+            <Typography variant="subtitle1">
+                ชำระเงิน
+            </Typography>
+            </div>
+            </div>
+            <div className=" w-200  bg-gray-200  shadow-lg md:drop-shadow-xl rounded-b-lg mx-10 mb-10 mt-0 shadow-black">
+            
+            <div className="flex space-x-28 ">
+            
+
+            {/* ฝั่งซ้าย */}
+            {/* หัว */}
+            <div className="flex flex-col space-y-4 mt-5 ml-40 items-center justify-center">
+                <Typography variant="body1">
+                แสกน QR code เพื่อชำระเงิน
+                </Typography>
+            {/* qr code */}
+            <div className="flex flex-wrap justify-center">
+            <img
+            src={qr}
+            class="p-1 bg-white border rounded-lg max-w-sm shadow-sm"
+            alt="..."
+            />
+            </div>
+
+            {/* ราคา */}
+            <div className="h-48 w-[500px] bg-green-300 mb-5 rounded-lg shadow-sm">
+                <div className="flex space-x-6">
+                    <div className="flex flex-col space-y-6 ml-5 mt-3">
+                    <Typography variant="body1">
+                        ราคา Post
+                        </Typography>
+                    <Typography variant="body1">
+                        ราคา Boost Post
+                    </Typography>
+                    <Typography variant="body1">
+                        รวมสุทธิ
+                    </Typography>
+                    </div>
+
+
+                    <div className="flex flex-col space-y-5 ml-5 mt-3">
+                    <div className="w-64 h-10 bg-white rounded-lg shadow-sm text-center">
+                    <Typography variant="body1">
+                        500
+                        </Typography>
+                        </div>
+                    <div className="w-64 h-10 bg-white rounded-lg shadow-sm text-center">
+                    <Typography variant="body1">
+                        Boost
+                        </Typography>
+                        </div>
+                    <div className="w-64 h-10 bg-white rounded-lg shadow-sm text-center">
+                    <Typography variant="body1">
+                        รวม
+                        </Typography>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* ฝั่งขวา */}
+            <div class="flex space-x-10">
+            <div class="flex flex-col space-y-7 mt-48">
+                <div>
+                <Typography variant="body1">
+                        ชื่อ-นามสกุล ผู้โอนเงิน
+                        </Typography> 
+                </div>
+                <div>
+                <Typography variant="body1">
+                        เวลาที่โอนเงิน
+                        </Typography>
+                </div>
+                <div>
+                <Typography variant="body1">
+                        แนบไฟล์**
+                        </Typography>
+                <Typography variant="body1">
+                        สลิปการชำระเงิน
+                        </Typography>
+                </div>
+                
+                </div>
+            <div class="flex flex-col space-y-7 mt-48">
+            <div className="w-full h-10 bg-white rounded-lg shadow-sm text-center">
+                    <Typography variant="body1">
+                        500
+                        </Typography>
+                        </div>
+                    <div className="w-full h-10 bg-white rounded-lg shadow-sm text-center">
+                    <Typography variant="body1">
+                        Boost
+                        </Typography>
+                        </div>
+                    <div className="h-10">
+                        <input class="form-control
+                        block
+                        w-full
+                        py-1.5
+                        text-base
+                        font-normal
+                        text-gray-700
+                        bg-white bg-clip-padding
+                        border border-solid border-gray-300
+                        rounded-lg
+                        transition
+                        ease-in-out
+                        m-0
+                        focus:text-gray-700 focus:bg-white focus:border-green-300 focus:outline-none" type="file" id="formFile"/>
+                    </div>
+
+                </div>
+            </div>
+            </div>
+            <div className="absolute right-10 bottom-2">
+            <button
+              class="bg-[#24AB82] drop-shadow-md font-bold text-white text-2xl rounded-xl px-6 py-2.5 mt-5 mb-4 hover:bg-[#1F795E] hover:ring-2 hover:ring-white focus:ring-2 focus:ring-white focus:outline-none "
+            >
+              <Typography variant="body1">
+              ชำระเงิน
+              </Typography>
+                  </button>
+            </div>
+            </div>
+        </ThemeProvider>
+    );
+}
--- src/page/Postjob/index.js
+++ src/page/Postjob/index.js
@@ -2,12 +2,10 @@ import React from "react";
 import Typography from '@material-ui/core/Typography';
 import { ThemeProvider, createTheme } from '@material-ui/core/styles';
 import "./Postjob.css";
-import { TextField } from "@material-ui/core";
 
 
 
-//class => คำสั่งการทำงาน
-//className => ชื่อ เผื่อใช้ไว้ดึงข้อมูลหรือใช้แต่ง css
+
 export default function Postjob(){
     const notosan1=createTheme({
         typography:{
@@ -37,60 +35,59 @@ export default function Postjob(){
         },
       });
     return(
+      
         <ThemeProvider theme={notosan1}>
-        <div class="h-screen w-200 bg-gray-200  drop-shadow-xl md:drop-shadow-xl rounded-lg mx-10 mt-8">
-        <div class=" h-20 w-200  bg-green-300 rounded-t-lg">
+        <div className=" h-20 w-200 bg-green-300 rounded-t-lg mx-10 mt-8">
 
             {/* หัวข้อ */}
-            <div class="pl-8 pt-2.5 ">
+            <div div className="pl-8 pt-2.5 ">
             <Typography variant="subtitle1">
                 สร้างโพสต์หาพนักงาน
             </Typography>
             </div>
+        </div>
 
+        <div className=" w-200  bg-gray-200  shadow-lg md:drop-shadow-xl rounded-b-lg mx-10 mb-10 shadow-black">
             {/* รายละเอียดงาน */}
-            <div class="pl-7 pt-3">
+            <div className="pl-7 pt-3">
             <Typography variant="body1">
                 รายละเอียดงาน
             </Typography>
-            
-            {/* <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"/> */}
+            {/* <label for="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"/> */}
             <textarea className="message" rows="4" class="resize-none p-2.5 w-11/12 ml-7 mt-2 rounded-lg ring-2 ring-black" placeholder="กรุณากรอกรายละเอียด..."></textarea>
         
             {/* สวัสดิการ */}
-            <div class="pt-3">
-                
+            <div className="pt-3">
             <Typography variant="body1">
                 สวัสดิการ
             </Typography>
-            
             </div>
             <textarea className="message" rows="4" class="resize-none p-2.5 w-11/12 ml-7 mt-2 rounded-lg ring-2 ring-black" placeholder="กรุณากรอกรายละเอียด..."></textarea>
             
 
             {/* คุณสมบัติของผู้สมัคร */}
-            <div class="pt-3">
+            <div className="pt-3">
             <Typography variant="body1">
                 คุณสมบัติของผู้สมัคร
             </Typography>
-            <div class="flex space-x-6 pt-2">
-                <div class="flex space-x-1">
+            <div className="flex space-x-6 pt-2">
+                <div className="flex space-x-2">
                 <Typography variant="body2">
                         มหาลัย
                 </Typography>
-                <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[120px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                <option>United States</option>
+                <select id="countries" className="text-black text-sm rounded-lg ring-2 ring-black focus:ring-black-500 focus:border-black-500 block w-[250px] p-2.5">
+                <option>สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง</option>
                 <option>Canada</option>
                 <option>France</option>
                 <option>Germany</option>
                 </select>
                 </div>
 
-                <div class="flex space-x-1">
+                <div className="flex space-x-2">
                 <Typography variant="body2">
                         คณะ
                 </Typography>
-                <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[120px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <select id="countries" className="text-black text-sm rounded-lg ring-2 ring-black focus:ring-black-500 focus:border-black-500 block w-[250px] p-2.5">
                 <option>United States</option>
                 <option>Canada</option>
                 <option>France</option>
@@ -98,11 +95,11 @@ export default function Postjob(){
                 </select>
                 </div>
 
-                <div class="flex space-x-1">
+                <div className="flex space-x-2">
                 <Typography variant="body2">
                         สาขา
                 </Typography>
-                <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[120px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <select id="countries" className="text-black text-sm rounded-lg ring-2 ring-black focus:ring-black-500 focus:border-black-500 block w-[250px] p-2.5">
                 <option>United States</option>
                 <option>Canada</option>
                 <option>France</option>
@@ -110,13 +107,130 @@ export default function Postjob(){
                 </select>
                 </div>
                 
+              </div>
+
+
+              <div className="flex space-x-6 pt-3">
+                <div className="flex space-x-2">
+                <Typography variant="body2">
+                        ประเภทงาน
+                </Typography>
+                <select id="countries" className="text-black text-sm rounded-lg ring-2 ring-black focus:ring-black-500 focus:border-black-500 block w-[250px] p-2.5">
+                <option>สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง</option>
+                <option>Canada</option>
+                <option>France</option>
+                <option>Germany</option>
+                </select>
+                </div>
+
+                <div className="flex space-x-2">
+                <Typography variant="body2">
+                        ตำแหน่ง
+                </Typography>
+                <select id="countries" className="text-black text-sm rounded-lg ring-2 ring-black focus:ring-black-500 focus:border-black-500 block w-[250px] p-2.5">
+                <option>United States</option>
+                <option>Canada</option>
+                <option>France</option>
+                <option>Germany</option>
+                </select>
+                </div>
+
+                {/* เช็คใน input ใส่ได้แค่เลข */}
+                <div className="flex space-x-2">
+                <Typography variant="body2">
+                        เงินเดือน
+                </Typography>
+                <input type="number"
+                className="text-black text-sm rounded-lg ring-2 ring-black focus:ring-black-500 focus:border-black-500 block w-[80px] p-2.5"
+                placeholder="ต่ำสุด"></input>
+                <Typography variant="body2">
+                        -
+                </Typography>
+                <input type="number"
+                className="text-black text-sm rounded-lg ring-2 ring-black focus:ring-black-500 focus:border-black-500 block w-[80px] p-2.5"
+                placeholder="สูงสุด"></input>
+                </div>
+                
+              </div>
             </div>
+
+
+            <div className="flex space-x-6 pt-12">
+                <div className="flex space-x-2">
+                <Typography variant="body2">
+                        อัตราที่รับ
+                </Typography>
+                <input type="number"
+                className="text-black text-sm rounded-lg ring-2 ring-black focus:ring-black-500 focus:border-black-500 block w-[80px] p-2.5"
+                placeholder="จำนวน"></input>
+                
+                </div>
+
+                <div className="flex space-x-2">
+                <Typography variant="body2">
+                        จังหวัดของสถานประกอบการ
+                </Typography>
+                <select id="countries" className="text-black text-sm rounded-lg ring-2 ring-black focus:ring-black-500 focus:border-black-500 block w-[250px] p-2.5">
+                <option>United States</option>
+                <option>Canada</option>
+                <option>France</option>
+                <option>Germany</option>
+                </select>
+                </div>
+
+                {/* เช็คใน input ใส่ได้แค่เลข */}
+                <div className="flex space-x-2">
+                <Typography variant="body2">
+                        ระยะเวลาการโพสต์
+                </Typography>
+                <select id="countries" className="text-black text-sm rounded-lg ring-2 ring-black focus:ring-black-500 focus:border-black-500 block w-[250px] p-2.5">
+                <option>United States</option>
+                <option>Canada</option>
+                <option>France</option>
+                <option>Germany</option>
+                </select>
+                </div>
+                
+              </div>
             </div>
             
+            
+            {/* สถานที่ประกอบการของบริษัท */}
+            <div className="pl-8 pt-3">
+            <Typography variant="body1">
+              สถานที่ประกอบการของบริษัท
+            </Typography>
+            </div>
+            <textarea className="message" rows="4" class="resize-none p-2.5 w-11/12 ml-7 mt-2 rounded-lg ring-2 ring-black" placeholder="กรุณากรอกรายละเอียด..."></textarea>
+
+            
+            <div class="flex items-center pl-8 pt-3">
+            <input id="checkbox-2" aria-describedby="checkbox-2" type="checkbox" class="w-4 h-4 text-black rounded ring-2 ring-gray-700 "/>
+            <label for="checkbox-2" class="ml-3 text-sm font-medium text-black">
+              <Typography variant="body1">
+              จ่ายเงินเพื่อ Boost Post
+              </Typography>
+              </label>
 
             </div>
-        </div>
-        </div>
-        </ThemeProvider>
+
+            <a href="/paymentcompany" class="text-decoration-none">
+            <div className="flex items-center justify-center">
+            <button
+              class="bg-[#24AB82] drop-shadow-md font-bold text-white text-2xl rounded-xl px-6 py-2.5 mt-5 mb-4 hover:bg-[#1F795E] hover:ring-2 hover:ring-white focus:ring-2 focus:ring-white focus:outline-none "
+            >
+              <Typography variant="body1">
+              หน้าต่อไป
+              </Typography>
+                  </button>
+            </div>
+            </a>
+
+
+            
+        
+      </div>
+      
+    </ThemeProvider>
     );
 }
